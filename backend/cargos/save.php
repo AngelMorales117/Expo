f352e2bd9026ed75d2430a5527f4d915f389db29
EdwinAlmira <ealopezalmira@gmail.com>
2016-05-28 16:34:55 -0600

    Cargo error al redirrecionar

--- backend/cargos/save.php
+++ backend/cargos/save.php
@@ -1,11 +1,11 @@
 <?php
-require("../lib/page.php");
-require("../../lib/database.php");
-require("../../lib/validator.php");
+require("../pagina.php");
+require("../procesos/database.php");
+require("../procesos/validator.php");
 
 if(empty($_GET['id'])) 
 {
-    Page::header("Agregar categoría");
+    Page::header("Agregar cargo");
     $id = null;
     $nombre = null;
     $descripcion = null;
@@ -14,23 +14,16 @@ else
 {
     Page::header("Modificar categoría");
     $id = $_GET['id'];
-    $sql = "SELECT * FROM categorias WHERE id_categoria = ?";
+    $sql = "SELECT * FROM cargos WHERE id_cargo = ?";
     $params = array($id);
     $data = Database::getRow($sql, $params);
-    $nombre = $data['nombre_categoria'];
-    $descripcion = $data['descripcion_categoria'];
+    $nombre = $data['cargos'];
 }
 
 if(!empty($_POST))
 {
     $_POST = Validator::validateForm($_POST);
   	$nombre = $_POST['nombre'];
-  	$descripcion = $_POST['descripcion'];
-    if($descripcion == "")
-    {
-        $descripcion = null;
-    }
-
     try 
     {
       	if($nombre == "")
@@ -40,39 +33,38 @@ if(!empty($_POST))
 
         if($id == null)
         {
-        	$sql = "INSERT INTO categorias(nombre_categoria, descripcion_categoria) VALUES(?, ?)";
-            $params = array($nombre, $descripcion);
+        	$sql = "INSERT INTO cargos(cargos) VALUES(?)";
+            $params = array($nombre);
         }
         else
         {
-            $sql = "UPDATE categorias SET nombre_categoria = ?, descripcion_categoria = ? WHERE id_categoria = ?";
-            $params = array($nombre, $descripcion, $id);
+            $sql = "UPDATE cargos SET cargos = ? WHERE Id_cargo = ?";
+            $params = array($nombre, $id);
         }
         Database::executeRow($sql, $params);
-        header("location: index.php");
+        header("location: cargos.php");
     }
     catch (Exception $error)
     {
-        print("<div class='card-panel red'><i class='material-icons left'>error</i>".$error->getMessage()."</div>");
+        print("<div class='container-fluid titulo'> <h3> ERROR </h3> </div>".$error->getMessage()."</div>");
     }
 }
 ?>
+<div class="container-fluid">
 <form method='post' class='row' enctype='multipart/form-data'>
     <div class='row'>
-        <div class='input-field col s12 m6'>
-          	<i class='material-icons prefix'>add</i>
+        <div class='col-md-6'>
+          	<i class='glyphicon glyphicon-pencil'></i>
           	<input id='nombre' type='text' name='nombre' class='validate' length='50' maxlenght='50' value='<?php print($nombre); ?>' required/>
           	<label for='nombre'>Nombre</label>
         </div>
-        <div class='input-field col s12 m6'>
-          	<i class='material-icons prefix'>description</i>
-          	<input id='descripcion' type='text' name='descripcion' class='validate' length='200' maxlenght='200' value='<?php print($descripcion); ?>'/>
-          	<label for='descripcion'>Descripción</label>
-        </div>
     </div>
-    <a href='index.php' class='btn grey'><i class='material-icons right'>cancel</i>Cancelar</a>
- 	<button type='submit' class='btn blue'><i class='material-icons right'>check_circle</i>Guardar</button>
+    <div class="btnforms">
+    <a href='cargos.php' class='btn btn-danger'><i class='glyphicon glyphicon-remove'></i>Cancelar</a>
+ 	<button type='submit' class='btn btn-primary'><i class='glyphicon glyphicon-ok'></i>Guardar</button>
+    </div>
 </form>
+</div>
 <?php
 Page::footer();
 ?>
